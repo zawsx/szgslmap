0bbafe4c011b01cacb95457e0dd72a7d1e76895c
Chris <zawsx@users.noreply.github.com>
2024-06-28 20:16:58 +0100

    sdfgasdf

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,3 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1q6QqUUmM48TutfjbSppK3pFD9jvYI4Jmv_B3Dcsst8k';
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyDw0BEpyjaTYtPQgvaKqb7PxQrLUYfEY0o';
+var gglnewkey='AIzaSyANUVzOSK7Njw0yfGA6C7Oe29RupF63uag';
