8b66db6bf455174aa47ebd71e8bfef18c4782e9b
Chris <zawsx@users.noreply.github.com>
2024-06-28 20:13:38 +0100

    Update map2.js

--- scripts/map2.js
+++ scripts/map2.js
@@ -4,15 +4,13 @@ $(window).on('load', function() { var documentSettings = {};  var group2color = 
   allTextLabelsLayers = [];  allTextLabels = [];
   /** Here all data processing from the spreadsheet happens  */
   var mapData;/** Triggers the load of the spreadsheet and map creation */
-
-        if (typeof googleApiKey !== 'undefined' && googleApiKey) {
-          var parse = function(res) {return Papa.parse(Papa.unparse(res[0].values), {header: true} ).data;}
-          var apiUrl = 'https://sheets.googleapis.com/v4/spreadsheets/'; var spreadsheetId = googleDocURL.indexOf('/d/') > 0 ? googleDocURL.split('/d/')[1].split('/')[0] : googleDocURL
-          $.getJSON(apiUrl + spreadsheetId + '?key=' + googleApiKey).then(function(data) {var sheets = data.sheets.map(function(o) { return o.properties.title })
-              if (sheets.length === 0 || !sheets.includes('Options')) {'Could not load data from the Google Sheet'}
-
-              // First, read 3 sheets: Options, Points, and Polylines
-              $.when( $.getJSON(apiUrl + spreadsheetId + '/values/Options?key=' + googleApiKey), $.getJSON(apiUrl + spreadsheetId + '/values/Points?key=' + googleApiKey),
+  if (typeof googleApiKey !== 'undefined' && googleApiKey) {
+     var parse = function(res) {return Papa.parse(Papa.unparse(res[0].values), {header: true} ).data;}
+     var apiUrl = 'https://sheets.googleapis.com/v4/spreadsheets/'; var spreadsheetId = googleDocURL.indexOf('/d/') > 0 ? googleDocURL.split('/d/')[1].split('/')[0] : googleDocURL
+     $.getJSON(apiUrl + spreadsheetId + '?key=' + googleApiKey).then(function(data) {
+        var sheets = data.sheets.map(function(o) { return o.properties.title }); if (sheets.length === 0 || !sheets.includes('Options')) {'Could not load data from the Google Sheet'}
+        // First, read 3 sheets: Options, Points, and Polylines
+        $.when( $.getJSON(apiUrl + spreadsheetId + '/values/Options?key=' + googleApiKey), $.getJSON(apiUrl + spreadsheetId + '/values/Points?key=' + googleApiKey),
                 $.getJSON(apiUrl + spreadsheetId + '/values/Polylines?key=' + googleApiKey) ).done(function(options, points, polylines) 
                  {var polygonSheets = sheets.filter(function(name) { return name.indexOf('Polygons') === 0}) // Which sheet names contain polygon data?
                 // Define a recursive function to fetch data from a polygon sheet
